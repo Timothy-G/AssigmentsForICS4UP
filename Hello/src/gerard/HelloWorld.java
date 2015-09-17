a6fd77be037cd04c4bc480f8ba55d2ed714b4d6f
322040049 <322040049@C-GP828J1.tdsb.on.ca>
2015-09-17 13:53:02 -0400

    Adding HelloWorld

---
+++ Hello/src/gerard/HelloWorld.java
@@ -0,0 +1,13 @@
+package gerard;
+
+/**
+ * Displays a "Hello World!" message on the screen
+ */
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+	}
+}
+
